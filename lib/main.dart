9b03d6770852182c168aa4c60aacca48c3bcb70d
junihoj <ezeonyekachukwu98@gmail.com>
2023-12-10 21:18:47 +0100

    created a new route new note and adjusted the Authservice test

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:test_project/constants/routes.dart';
 import 'package:test_project/services/auth/auth_service.dart';
 import 'package:test_project/views/login_view.dart';
-import 'package:test_project/views/notes_view.dart';
+import 'package:test_project/views/notes/new_note_view.dart';
+import 'package:test_project/views/notes/notes_view.dart';
 import 'package:test_project/views/register_view.dart';
 import 'package:test_project/views/verify_email_view.dart';
 
@@ -19,7 +20,8 @@ void main() {
       loginRoute: (context) => const LoginView(),
       registerRoute: (context) => const RegisterView(),
       notesRoute: (context) => const NotesView(),
-      verifyEmailRoute: (content) => const VerifyEmailView(),
+      verifyEmailRoute: (context) => const VerifyEmailView(),
+      newNoteRoute: (context) => const NewNoteView()
     },
   ));
 }
