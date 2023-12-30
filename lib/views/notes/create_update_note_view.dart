fa32b7c34e6949620ec01010842be64b59aa78c6
junihoj <ezeonyekachukwu98@gmail.com>
2023-12-31 00:25:11 +0100

    clean build and share

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:test_project/services/auth/auth_service.dart';
 import 'package:test_project/services/cloud/cloud_note.dart';
 import 'package:test_project/services/cloud/firebase_cloud_storage.dart';
+import 'package:test_project/utilities/dialogs/cannot_share_empty_note_dialog.dart';
 import 'package:test_project/utilities/generics/get_arguments.dart';
 
 class CreateUpdateNoteView extends StatefulWidget {
@@ -84,6 +86,19 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     return Scaffold(
         appBar: AppBar(
           title: const Text("New Note"),
+          actions: [
+            IconButton(
+              onPressed: () async {
+                final text = _textController.text;
+                if (_note == null || text.isEmpty) {
+                  await showCannotShareEmptyNoteDialog(context);
+                } else {
+                  Share.share(text);
+                }
+              },
+              icon: const Icon(Icons.share),
+            ),
+          ],
         ),
         body: FutureBuilder(
             future: createOrGetExistingNote(context),
